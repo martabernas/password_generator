9fb368f09397b4c9d0543a5ee804e3af4e4b2242
Marta Bernas <marta.bernas1996@gmail.com>
2023-02-18 16:15:00 +0100

    Set default number of characters

--- main.py
+++ main.py
@@ -3,29 +3,29 @@ import random
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
     letters_lowercase = list(string.ascii_lowercase)
     letters_uppercase = list(string.ascii_uppercase)
     digits = list(string.digits)
     special_characters = list(string.punctuation)
     new_password = 'Y'
     proper_nums = str(list(range(0, 11)))
+    default_num = "3"
 
     print("Hi, Marta. Welcome to GenPas!")
     while True:
-        if new_password == 'Y':
+        if new_password == 'Y' or new_password == 'y':
             password = []
-            num_of_letters_lowercase = input("Desired number of lowercase letters: ") or "5"
-            num_of_letters_uppercase = input("Desired number of uppercase letters: ") or "1"
-            num_of_digits = input("Desired number of digits: ") or "1"
-            num_of_special_characters = input("Desired number of special characters: ") or "1"
+            print(f"Desired number (default {default_num}) of: ")
+            num_of_letters_lowercase = input("* lowercase letters: ") or default_num
+            num_of_letters_uppercase = input("* uppercase letters: ") or default_num
+            num_of_digits = input("* digits: ") or default_num
+            num_of_special_characters = input("* special characters: ") or default_num
 
-            if num_of_letters_lowercase not in proper_nums or num_of_letters_uppercase not in proper_nums or \
-                num_of_digits not in proper_nums or num_of_special_characters not in proper_nums:
-                print("Numbers must by integers in range from 0 to 10!")
+            if num_of_letters_lowercase not in proper_nums or num_of_letters_uppercase not in \
+                    proper_nums or num_of_digits not in proper_nums or num_of_special_characters \
+                    not in proper_nums:
+                print("Numbers must be integers in range from 0 to 10!")
                 continue
-
-
 
             password.extend(random.choices(letters_lowercase, k=int(num_of_letters_lowercase)))
             password.extend(random.choices(letters_uppercase, k=int(num_of_letters_uppercase)))
@@ -36,10 +36,11 @@ def print_hi(name):
             print(f"Your password: {''.join(password)}")
             new_password = input("Do you need a new password? Y/N:")
 
-        elif new_password == 'N':
+        elif new_password == 'N' or new_password == 'n':
             break
         else:
             new_password = input("Do you need a new password? Y/N:")
+
 
 if __name__ == '__main__':
     print_hi('PyCharm')
